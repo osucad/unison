b66da633c3d6d8b5778ef5803b0ad506c4106b30
Marvin Schürz <m.schuerz@hautzy.com>
2025-04-15 03:19:24 +0200

    crate in-memory summary storage

---
+++ apps/unison-server/src/services/IDocumentStorage.ts
@@ -0,0 +1,9 @@
+import { IDocumentSummary } from "@unison/protocol";
+
+export interface IDocumentStorage {
+  getSummary(documentId: string, sequenceNumber: number | 'latest'): Promise<IDocumentSummary | null>
+
+  appendSummary(documentId: string, summary: IDocumentSummary): Promise<void>
+
+  createDocument(summary: Omit<IDocumentSummary, 'sequenceNumber'>): Promise<string>
+}
