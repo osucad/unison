2fb1175524059676e3439b5a7f81c8770b3d6342
Marvin Schürz <m.schuerz@hautzy.com>
2025-04-16 12:44:54 +0200

    Add stylistic rules to eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,69 +1,77 @@
 import nx from "@nx/eslint-plugin";
+import stylistic from '@stylistic/eslint-plugin'
 
 export default [
-    ...nx.configs["flat/base"],
-    ...nx.configs["flat/typescript"],
-    ...nx.configs["flat/javascript"],
-    {
-      "ignores": [
-        "**/dist",
-        "**/vite.config.*.timestamp*",
-        "**/vitest.config.*.timestamp*"
-      ]
-    },
-    {
-        files: [
-            "**/*.ts",
-            "**/*.tsx",
-            "**/*.js",
-            "**/*.jsx"
-        ],
-        rules: {
-            "@nx/enforce-module-boundaries": [
-                "error",
-                {
-                    enforceBuildableLibDependency: true,
-                    allow: [
-                        "^.*/eslint(\\.base)?\\.config\\.[cm]?js$"
-                    ],
-                    depConstraints: [
-                        {
-                            sourceTag: "scope:shared",
-                            onlyDependOnLibsWithTags: [
-                                "scope:shared"
-                            ]
-                        },
-                        {
-                            sourceTag: "scope:server",
-                            onlyDependOnLibsWithTags: [
-                                "scope:server",
-                                "scope:shared",
-                            ]
-                        },
-                        {
-                            sourceTag: "scope:client",
-                            onlyDependOnLibsWithTags: [
-                                "scope:client",
-                                "scope:shared",
-                            ]
-                        }
-                    ]
-                }
-            ]
+  ...nx.configs["flat/base"],
+  ...nx.configs["flat/typescript"],
+  ...nx.configs["flat/javascript"],
+  {
+    "ignores": [
+      "**/dist",
+      "**/vite.config.*.timestamp*",
+      "**/vitest.config.*.timestamp*"
+    ]
+  },
+  {
+    files: [
+      "**/*.ts",
+      "**/*.tsx",
+      "**/*.js",
+      "**/*.jsx"
+    ],
+    rules: {
+      "@nx/enforce-module-boundaries": [
+        "error",
+        {
+          enforceBuildableLibDependency: true,
+          allow: [
+            "^.*/eslint(\\.base)?\\.config\\.[cm]?js$"
+          ],
+          depConstraints: [
+            {
+              sourceTag: "scope:shared",
+              onlyDependOnLibsWithTags: [
+                "scope:shared"
+              ]
+            },
+            {
+              sourceTag: "scope:server",
+              onlyDependOnLibsWithTags: [
+                "scope:server",
+                "scope:shared",
+              ]
+            },
+            {
+              sourceTag: "scope:client",
+              onlyDependOnLibsWithTags: [
+                "scope:client",
+                "scope:shared",
+              ]
+            },
+          ]
         }
-    },
-    {
-        files: [
-            "**/*.ts",
-            "**/*.tsx",
-            "**/*.cts",
-            "**/*.mts",
-            "**/*.js",
-            "**/*.jsx",
-            "**/*.cjs",
-            "**/*.mjs"
-        ],
-        // Override or add rules here
-        rules: {}
+      ]
     }
+  },
+  {
+    files: [
+      "**/*.ts",
+      "**/*.tsx",
+      "**/*.cts",
+      "**/*.mts",
+      "**/*.js",
+      "**/*.jsx",
+      "**/*.cjs",
+      "**/*.mjs"
+    ],
+    plugins: {
+      "@stylistic": stylistic,
+    },
+    rules: {
+      "@stylistic/indent": ["error", 2],
+      "@stylistic/semi": ["error", "always"],
+      "@stylistic/brace-style": ["error", "allman"],
+      "@stylistic/member-delimiter-style": ["error"]
+    }
+  }
 ];
