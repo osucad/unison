9c184a70218d055f770de8482ab650a03b5660ee
Marvin Schürz <m.schuerz@hautzy.com>
2025-04-22 10:53:01 +0200

    Get rid of top-level await

--- apps/client-playground/src/main.ts
+++ apps/client-playground/src/main.ts
@@ -1,17 +1,22 @@
 import { UnisonClient } from "@unison-client/core";
 import { Beatmap, BeatmapMetadata } from "./Beatmap";
 
-const client = new UnisonClient("http://localhost:3333");
+async function start() 
+{
+  const client = new UnisonClient("http://localhost:3333");
 
-const document = await client.create({
-  schema: {
-    beatmap: Beatmap,
-  },
-  types: [
-    Beatmap,
-    BeatmapMetadata,
-  ]
-});
+  const document = await client.create({
+    schema: {
+      beatmap: Beatmap,
+    },
+    types: [
+      Beatmap,
+      BeatmapMetadata,
+    ]
+  });
 
-const { beatmap } = document.root;
+  const { beatmap } = document.root;
 //      ^?
+}
+
+void start();
